487d3f4d93253de00f58c8bce31bd696b5b3cb72
Tanvir 321110 <tanvirahmed321110@gmail.com>
2026-06-25 12:49:22 +0600

    js added

--- static/src/js/home.js
+++ static/src/js/home.js
@@ -2,16 +2,63 @@ const gridBtn = document.getElementById('gridBtn');
 const listBtn = document.getElementById('listBtn');
 const contentsWrapper = document.getElementById('grid-list-contents');
 
-// লিস্ট ভিউ বাটনে ক্লিক করলে
+
+//list btn
 listBtn.addEventListener('click', () => {
     gridBtn.classList.remove('active');
     listBtn.classList.add('active');
-    contentsWrapper.classList.add('list-contents'); // লিস্ট ক্লাসে রূপান্তর
+    contentsWrapper.classList.add('list-contents');
 });
 
-// গ্রিড ভিউ বাটনে ক্লিক করলে
+
+//grid btn
 gridBtn.addEventListener('click', () => {
     listBtn.classList.remove('active');
     gridBtn.classList.add('active');
-    contentsWrapper.classList.remove('list-contents'); // গ্রিড ক্লাসে ফেরত নেওয়া
+    contentsWrapper.classList.remove('list-contents');
 });
+
+
+
+
+
+
+
+
+// ==========  For Popup  ===========
+document.addEventListener("DOMContentLoaded", function () {
+    document.addEventListener("click", function (ev) {
+        const button = ev.target.closest(".add_to_cart");
+
+        if (!button) {
+            return;
+        }
+
+        ev.preventDefault();
+        const productId = button.dataset.productId;
+
+        showB2BCartPopup(
+            "Added to Cart",
+            "Product added to draft order successfully."
+        );
+    });
+});
+
+function showB2BCartPopup(title, message) {
+    const popup = document.getElementById("b2bCartPopup");
+    const titleEl = document.getElementById("b2bCartPopupTitle");
+    const messageEl = document.getElementById("b2bCartPopupMessage");
+
+    if (!popup || !titleEl || !messageEl) {
+        return;
+    }
+
+    titleEl.innerText = title;
+    messageEl.innerText = message;
+
+    popup.classList.add("show");
+
+    setTimeout(function () {
+        popup.classList.remove("show");
+    }, 3500);
+}
