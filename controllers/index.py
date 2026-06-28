f58e347f7240732b151fff0f8d662691098cf34c
tanvi <tanvirahmed321110@gmail.com>
2026-06-28 15:23:49 +0600

    sort added

--- controllers/index.py
+++ controllers/index.py
@@ -6,15 +6,24 @@ from odoo.tools.image import image_data_uri
 class TimeAccessPortal(http.Controller):
     @http.route('/index', type='http', auth='user', website=True)
 
-    def index_f(self, **kw):
+    def index_f(self,sort='default', **kw):
 
-        products = request.env['product.template'].sudo().search([
+        domain = [
             ('is_b2b_portal', '=', True)
-        ])
+        ]
 
+        #product sort desc and asc
+        order = 'id desc'
+        if sort == 'low-high':
+            order = 'list_price asc'
+        elif sort == 'high-low':
+            order = 'list_price desc'
+
+        products = request.env['product.template'].sudo().search(domain,order=order)
 
         return request.render('time_access_portal.index_page', {
             'active_menu': 'products',
             'products':products,
+            'sort':sort,
         })
 
--- controllers/orders.py
+++ controllers/orders.py
@@ -14,7 +14,7 @@ class TimeAccessPortal(http.Controller):
 
     # ===================  Orders Details Page  ====================
     @http.route('/orders/order-details', type='http', auth='user', website=True)
-    def index_f(self, **kw):
+    def order_details_f(self, **kw):
         return request.render('time_access_portal.order_details_page', {
             'active_menu': 'orders',
         })
