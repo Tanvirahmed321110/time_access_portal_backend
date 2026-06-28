1ecb08b4c0b428d6184632729c97b458a249655e
tanvi <tanvirahmed321110@gmail.com>
2026-06-28 14:40:54 +0600

    +

--- controllers/__init__.py
+++ controllers/__init__.py
@@ -3,3 +3,4 @@ from . import cart
 from . import checkout
 from . import profile
 from . import order_invoice
+from . import orders
---
+++ controllers/orders.py
@@ -0,0 +1,21 @@
+from odoo import http
+from odoo.http import request
+
+
+class TimeAccessPortal(http.Controller):
+
+    # ===================  Orders Page  ====================
+    @http.route('/orders', type='http', auth='user', website=True)
+    def index_f(self, **kw):
+        return request.render('time_access_portal.orders_page', {
+            'active_menu': 'orders',
+        })
+
+
+    # ===================  Orders Details Page  ====================
+    @http.route('/orders/order-details', type='http', auth='user', website=True)
+    def index_f(self, **kw):
+        return request.render('time_access_portal.order_details_page', {
+            'active_menu': 'orders',
+        })
+
