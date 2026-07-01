fe5ca0844db022d6c2173166e052a1ea02eab823
tanvi <tanvirahmed321110@gmail.com>
2026-07-01 18:50:33 +0600

    ++

--- controllers/orders.py
+++ controllers/orders.py
@@ -1,5 +1,6 @@
 from odoo import http
 from odoo.http import request
+from odoo.addons.time_access_portal.utilitis.pagination import get_pager
 
 
 class TimeAccessPortal(http.Controller):
@@ -9,34 +10,52 @@ class TimeAccessPortal(http.Controller):
     def index_f(self, **kw):
         partner = request.env.user.partner_id
 
-        orders = request.env['sale.order'].sudo().search([
-            ('partner_id','=', partner.id),
-            ('state', 'in', ['sale','done','cancel']),
-        ],order='id desc')
+        domain = [
+            ('partner_id', '=', partner.id),
+            ('state', 'in', ['sale', 'done', 'cancel']),
+        ]
+
+        # ===== Pagination Setup =====
+        per_page = int(kw.get('per_page', 15))
+        total = request.env['sale.order'].sudo().search_count(domain)
+
+        pager = get_pager(
+            url='/orders',
+            total=total,
+            page=kw.get('page', 1),
+            per_page=per_page,
+        )
+
+        orders = request.env['sale.order'].sudo().search(
+            domain,
+            order='id desc',
+            offset=pager['offset'],
+            limit=pager['per_page']
+        )
 
         return request.render('time_access_portal.orders_page', {
             'active_menu': 'orders',
-            'orders' : orders
+            'orders': orders,
+            'pager': pager,
+            'total': total,
         })
 
 
     # ===================  Orders Details Page  ====================
     @http.route('/orders/<int:order_id>', type='http', auth='user', website=True)
-    def order_details_f(self,order_id ,**kw ):
+    def order_details_f(self, order_id, **kw):
         partner = request.env.user.partner_id
 
         order = request.env['sale.order'].sudo().search([
-            ('id','=', order_id),
-            ('partner_id','=', partner.id),
+            ('id', '=', order_id),
+            ('partner_id', '=', partner.id),
         ])
 
-        # delivery date
         delivery_date = None
         if order:
-            # এই সেলস অর্ডারের আন্ডারে যে ডেলিভারি চালানগুলো (Pickings) আছে তা খোঁজা হচ্ছে
             picking = request.env['stock.picking'].sudo().search([
                 ('sale_id', '=', order.id),
-                ('state', '=', 'done')  # শুধুমাত্র সফলভাবে ডেলিভারি হওয়া চালান
+                ('state', '=', 'done')
             ], limit=1, order='date_done desc')
 
             if picking and picking.date_done:
@@ -46,6 +65,5 @@ class TimeAccessPortal(http.Controller):
             'active_menu': 'orders',
             'order': order,
             'order_lines': order.order_line if order else [],
-            'delivery_date':delivery_date
+            'delivery_date': delivery_date
         })
-
