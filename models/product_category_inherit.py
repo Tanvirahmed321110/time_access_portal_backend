7e88e3e1249d783eebb6526b481f280e3b085ddc
tanvi <tanvirahmed321110@gmail.com>
2026-07-05 17:04:08 +0600

    updatae some field

--- models/product_category_inherit.py
+++ models/product_category_inherit.py
@@ -5,4 +5,5 @@ class ProductCategoryInherit(models.Model):
     _inherit = 'product.category'
 
     is_b2b_category = fields.Boolean(string='Is B2B Category?')
+    b2b_quantity = fields.Integer(string='B2B Quantity')
 
--- models/product_template_inherit.py
+++ models/product_template_inherit.py
@@ -5,64 +5,6 @@ class ProductProductInherit(models.Model):
     _inherit = 'product.template'
     _order = 'create_date desc'
 
-    is_b2b_portal = fields.Boolean(string='is b2b portal?')
-
-    # # For Userlink links page
-    # live_link  = fields.Char(string='Live Link')
-    # github_link = fields.Char(string='Github Repo Link')
-    # dev_link = fields.Char(string='Dev Server Link')
-    #
-    #
-    # custom_priority = fields.Selection([
-    #     ('normal','normal'),
-    #     ('medium','medium'),
-    #     ('high','high'),
-    #     ('urgent','urgent'),
-    # ], string=' Priority',default='normal')
-    #
-    # custom_status = fields.Selection([
-    #     ('planning', 'Planning'),
-    #     ('in_progress', 'In Progress'),
-    #     ('review', 'Under Review'),
-    #     ('completed', 'Completed'),
-    #     ('cancelled', 'Cancelled'),
-    # ], string='Project Status', default='planning')
-    #
-    # # assign members
-    # assigned_user_ids = fields.Many2many(
-    #     'res.users',
-    #     string='Assigned Users'
-    # )
-    #
-    # # 🔥 Remaining Days Field
-    # remaining_days = fields.Integer(
-    #     string='Remaining Days',
-    #     compute='_compute_remaining_days',
-    #     store=True,
-    #     readonly=True,
-    # )
-    #
-    # @api.depends('date')
-    # def _compute_remaining_days(self):
-    #     today = date.today()
-    #
-    #     for rec in self:
-    #         if rec.date:
-    #             rec.remaining_days = (rec.date - today).days
-    #         else:
-    #             rec.remaining_days = 0
-    #
-    # allocated_hours = fields.Float(
-    #     compute="_compute_allocated_hours",
-    #     store=True,
-    #     readonly=False
-    # )
-
-    # @api.depends("date_start", "date")
-    # def _compute_allocated_hours(self):
-    #     for rec in self:
-    #         if rec.date_start and rec.date:
-    #             delta = rec.date - rec.date_start
-    #             rec.allocated_hours = delta.days * 8
-    #         else:
-    #             rec.allocated_hours = 0.0
+    is_b2b_portal = fields.Boolean(string='is b2b portal')
+    b2b_price = fields.Float(string='B2B  Price')
+    b2b_qty = fields.Integer(string='B2B  Qty')
