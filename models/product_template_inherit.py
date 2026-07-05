fb6288ba9686278fc8a80f5c321b0a8ff92ec1f3
tanvi <tanvirahmed321110@gmail.com>
2026-07-05 18:44:51 +0600

    group creted

--- models/product_template_inherit.py
+++ models/product_template_inherit.py
@@ -6,5 +6,8 @@ class ProductProductInherit(models.Model):
     _order = 'create_date desc'
 
     is_b2b_portal = fields.Boolean(string='is b2b portal')
-    b2b_price = fields.Float(string='B2B  Price')
+    b2b_price = fields.Float(
+        string='B2B  Price',
+        groups='time_access_portal.group_b2b_management'
+    )
     b2b_qty = fields.Integer(string='B2B  Qty')
